6e75930abce4fc7159bcdd123e1b475ec0d5dc33
David Brooks <david@brooksphere.com>
2015-03-20 10:48:00 -0400

    Updated UserTest

--- MillerConstructionTests/UserTest.m
+++ MillerConstructionTests/UserTest.m
@@ -9,6 +9,13 @@
 #import <XCTest/XCTest.h>
 #import "User.h"
 
+#define TEST_USERNAME @"TestUsername"
+#define TEST_PASSWORD @"TestPassword"
+#define HASHED_PASSWORD @"7bcf9d89298f1bfae16fa02ed6b61908fd2fa8de45dd8e2153a3c47300765328"
+#define USERNAME @"djbrooks"
+#define PASSWORD @"ylxwpliyok"
+#define WRONG @"wrong"
+
 @interface UserTest : XCTestCase
 
 @end
@@ -26,25 +33,25 @@
 }
 
 -(void)testUserCreation {
-    User *user = [[User alloc] initWithUsername:@"TestUsername" andPassword:@"TestPassword"];
-    XCTAssertEqualObjects(@"TestUsername", [user username], @"User's username should be TestUsername");
-    XCTAssertEqualObjects(@"7bcf9d89298f1bfae16fa02ed6b61908fd2fa8de45dd8e2153a3c47300765328", [user password], @"User's password should be 7bcf9d89298f1bfae16fa02ed6b61908fd2fa8de45dd8e2153a3c47300765328, which is TestPassword hashed");
+    User *user = [[User alloc] initWithUsername:TEST_USERNAME andPassword:TEST_PASSWORD];
+    XCTAssertEqualObjects(TEST_USERNAME, [user username], @"User's username should be TestUsername");
+    XCTAssertEqualObjects(HASHED_PASSWORD, [user password], @"User's password should be 7bcf9d89298f1bfae16fa02ed6b61908fd2fa8de45dd8e2153a3c47300765328, which is TestPassword hashed");
 }
 
 -(void)testSuccessfulLogin {
-    User *user = [[User alloc] initWithUsername:@"djbrooks" andPassword:@"ylxwpliyok"];
+    User *user = [[User alloc] initWithUsername:USERNAME andPassword:PASSWORD];
     UserLoginType result = [user login];
     XCTAssertEqual(SuccessfulLogin, result, @"User's login should return SuccessfulLogin");
 }
 
 -(void)testIncorrectPassword {
-    User *user = [[User alloc] initWithUsername:@"djbrooks" andPassword:@"wrong"];
+    User *user = [[User alloc] initWithUsername:USERNAME andPassword:WRONG];
     UserLoginType result = [user login];
     XCTAssertEqual(IncorrectPassword, result, @"User's login should return IncorrectPassword");
 }
 
 -(void)testWrongUsername {
-    User *user = [[User alloc] initWithUsername:@"wrong" andPassword:@"wrong"];
+    User *user = [[User alloc] initWithUsername:WRONG andPassword:WRONG];
     UserLoginType result = [user login];
     XCTAssertEqual(WrongUsername, result, @"User's login should return WrongUsername");
 }
